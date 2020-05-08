ec91119b5e520e79a2c73ed19fa0c5bade1966ba
Lorenzo Tabares <10tabareslorenzo@gmail.com>
2020-05-08 15:46:53 -0400

    heroku

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.listen(PORT);
 
 if(process.env.NODE_ENV === 'production')
 {
-      app.use(express.static('../cleint/build'));
+      app.use(express.static('cleint/build'));
       app.get('*', (req,res) => {
             res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
       });
